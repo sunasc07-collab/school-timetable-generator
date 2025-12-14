02402b77e4e8a2a87c3df44483caa48c0ca7ef7f
Sunday Moses <sunasc07@gmail.com>
2025-12-14 23:39:58 +0000

    remove break and ASSEMBLY from the time table

--- src/context/timetable-provider.tsx
+++ src/context/timetable-provider.tsx
@@ -34,11 +34,9 @@ const DEFAULT_TIME_SLOTS: TimeSlot[] = [
     { period: 1, time: "8:00-8:40" },
     { period: 2, time: "8:40-9:20" },
     { period: 3, time: "9:20-10:00" },
-    { period: null, time: "10:00-10:20", isBreak: true, label: "SHORT BREAK" },
     { period: 4, time: "10:20-11:00" },
     { period: 5, time: "11:00-11:40" },
     { period: 6, time: "11:40-12:20" },
-    { period: null, time: "13:00-13:50", isBreak: true, label: "LONG BREAK" },
     { period: 7, time: "13:50-14:25" },
     { period: 8, time: "14:25-15:00" },
     { period: 9, time: "15:00-15:30" },
@@ -725,5 +723,7 @@ export const useTimetable = (): TimetableContextType => {
   return context;
 };
 
+
+    
 
     

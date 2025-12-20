e2fe6f0505af381cea9063327380f89ce511412f
Sunday Moses <sunasc07@gmail.com>
2025-12-20 18:56:23 +0000

    text "short break" in capital letters in between period 3 and period 4 a

--- src/context/timetable-provider.tsx
+++ src/context/timetable-provider.tsx
@@ -33,12 +33,13 @@ const DEFAULT_TIMESLOTS: TimeSlot[] = [
     { period: 1, time: '8:00-8:40' },
     { period: 2, time: '8:40-9:20' },
     { period: 3, time: '9:20-10:00' },
-    { period: 4, time: '10:00-10:40' },
-    { period: 5, time: '10:40-11:20' },
-    { period: 6, time: '11:20-12:00' },
-    { period: 7, time: '12:00-12:40' },
-    { period: 8, time: '12:40-13:20' },
-    { period: 9, time: '13:20-14:00' },
+    { period: null, time: '10:00-10:20', isBreak: true, label: 'SHORT BREAK' },
+    { period: 4, time: '10:20-11:00' },
+    { period: 5, time: '11:00-11:40' },
+    { period: 6, time: '11:40-12:20' },
+    { period: 7, time: '12:20-13:00' },
+    { period: 8, time: '13:00-13:40' },
+    { period: 9, time: '13:40-14:20' },
 ];
 
 
@@ -672,5 +673,7 @@ export const useTimetable = (): TimetableContextType => {
   return context;
 };
 
+
+    
 
     

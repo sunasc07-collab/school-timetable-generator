8da2a50d22e32826a0d9c7af56941779aa92628c
Sunday Moses <sunasc07@gmail.com>
2025-12-21 14:41:03 +0000

    revert to column lines and remove all the row lines. make line invisible

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -190,10 +190,10 @@ export default function Header() {
             },
             willDrawCell: (data: any) => {
                  data.cell.styles.lineWidth = {
-                    left: 0,
-                    right: 0,
-                    top: 0.1,
-                    bottom: 0.1,
+                    top: 0,
+                    bottom: 0,
+                    left: 0.1,
+                    right: 0.1,
                 };
             },
             didDrawCell: (data: any) => {

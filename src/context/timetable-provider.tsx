6fa0f68d3831b6e22226624e6feeeeffc1d0f40e
Sunday Moses <sunasc07@gmail.com>
2025-12-21 13:12:35 +0000

    make period 7 35mins. period 9 35mins

--- src/context/timetable-provider.tsx
+++ src/context/timetable-provider.tsx
@@ -36,10 +36,10 @@ const DEFAULT_TIMESLOTS: TimeSlot[] = [
     { period: 4, time: '10:00-10:20', isBreak: true, label: 'SHORT-BREAK' },
     { period: 5, time: '10:20-11:00' },
     { period: 6, time: '11:00-11:40' },
-    { period: 7, time: '11:40-12:20' },
-    { period: null, time: '12:20-13:10', isBreak: true, label: 'LUNCH' },
-    { period: 8, time: '13:10-13:50' },
-    { period: 9, time: '13:50-14:20' },
+    { period: 7, time: '11:40-12:15' },
+    { period: null, time: '12:15-13:05', isBreak: true, label: 'LUNCH' },
+    { period: 8, time: '13:05-13:45' },
+    { period: 9, time: '13:45-14:20' },
     { period: 10, time: '14:20-14:50' },
 ];
 
@@ -680,3 +680,5 @@ export const useTimetable = (): TimetableContextType => {
   }
   return context;
 };
+
+    
